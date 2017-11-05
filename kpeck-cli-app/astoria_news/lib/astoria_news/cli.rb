9bb32b19f0b5ac2c24e47b1af9804be9c9c4f86b
Kristen Peck <kpeck71@gmail.com>
2017-11-05 16:12:24 +0000

    added scrape method to collect all article types - food, news, deals)

--- kpeck-cli-app/astoria_news/lib/astoria_news/cli.rb
+++ kpeck-cli-app/astoria_news/lib/astoria_news/cli.rb
@@ -1,19 +1,25 @@
 class AstoriaNews::CLI
   def call
-    list article
+    list_article
     options
     goodbye
   end
 
   def list_article
     puts <<-HEREDOC
-    This is a title
+    1. This is a title
+    This is a short description
+    2. This is another title
     This is a short description
     HEREDOC
   end
 
   def options
     puts "Please enter a number to read more."
+    read_article
+  end
+
+  def read_article
   end
 
   def goodbye
--- kpeck-cli-app/astoria_news/lib/astoria_news/news_scraper.rb
+++ kpeck-cli-app/astoria_news/lib/astoria_news/news_scraper.rb
@@ -1,2 +1,19 @@
 class AstoriaNews::NewsScraper
+
+  def self.scrape
+    all = []
+    all << self.scrape_news
+    all << self.scrape_food
+    all << self.scrape_deals
+  end
+
+  def scrape_news
+  end
+
+  def scrape_food
+  end
+
+  def scrape_deals
+  end
+
 end

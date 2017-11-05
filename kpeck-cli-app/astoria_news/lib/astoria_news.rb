09c8e7ef0cca0cd505e2a9f93092b9dce05aea91
Kristen Peck <kpeck71@gmail.com>
2017-11-05 16:00:12 +0000

    adding pseudo code to CLI

--- kpeck-cli-app/astoria_news/lib/astoria_news.rb
+++ kpeck-cli-app/astoria_news/lib/astoria_news.rb
@@ -2,9 +2,9 @@ require "open-uri"
 require "nokogiri"
 require "pry"
 
-require "astoria_news/version"
-require "astoria_news/cli"
-require "astoria_news/news_scraper"
+require_relative "./astoria_news/version"
+require_relative "./astoria_news/cli"
+require_relative "./astoria_news/news_scraper"
 
 module AstoriaNews
   # Your code goes here...
--- kpeck-cli-app/astoria_news/lib/astoria_news/cli.rb
+++ kpeck-cli-app/astoria_news/lib/astoria_news/cli.rb
@@ -1,5 +1,23 @@
 class AstoriaNews::CLI
   def call
+    list article
+    options
+    goodbye
+  end
+
+  def list_article
+    puts <<-HEREDOC
+    This is a title
+    This is a short description
+    HEREDOC
+  end
+
+  def options
+    puts "Please enter a number to read more."
+  end
+
+  def goodbye
+    puts "Goodbye! Check back again tomorrow for more Astoria news."
   end
 
 end

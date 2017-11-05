8783e3c5f766c0fb52366e8ff7203f3ac8feeccc
Kristen Peck <kpeck71@gmail.com>
2017-11-05 15:45:01 +0000

    adding dependecides

--- kpeck-cli-app/astoria_news/lib/astoria_news.rb
+++ kpeck-cli-app/astoria_news/lib/astoria_news.rb
@@ -1,4 +1,10 @@
+require "open-uri"
+require "nokogiri"
+require "pry"
+
 require "astoria_news/version"
+require "astoria_news/cli"
+require "astoria_news/news_scraper"
 
 module AstoriaNews
   # Your code goes here...
